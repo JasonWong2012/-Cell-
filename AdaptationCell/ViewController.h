d5635a5709b58a02e0f7eb855f39bbbf8b8c903a
hans <841870973@qq.com>
2015-10-27 14:34:05 +0800

    Initial Commit

---
+++ AdaptationCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdaptationCell
+//
+//  Created by hans on 15/10/27.
+//  Copyright © 2015年 hans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
